6817c4661279d5a3f398e5fa5255d6bdb04f4c87
tintedsky <hongbonew@gmail.com>
2017-10-26 17:47:55 -0400

    1. Aspect Ratio Calculator 2. In .m file, you have to create the extension like the following to accommodate the IBOutlets:    @interface VideoCell()    ........//here you can drag and create the IBOutlets    @end

---
+++ DevslopeTurtorial/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  DevslopeTurtorial
+//
+//  Created by Hongbo Niu on 2017-10-26.
+//  Copyright © 2017 Udemy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
