7fae21b999eaa96b74255ee5ecb574dc7a284095
tintedsky <hongbonew@gmail.com>
2017-11-03 15:52:43 -0400

    1. Now the app is working fine. 2. Can reorganize the layout dynamically, but now perfect. 3. Ready for next Lecture: 228

--- DevslopeTurtorial/VideoVC.m
+++ DevslopeTurtorial/VideoVC.m
@@ -24,14 +24,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.webView.delegate = self;
+    
     self.titleLbl.text = self.video.videoTitle;
     self.descLbl.text = self.video.videoDescription;
     [self.webView loadHTMLString:self.video.videoIframe baseURL:nil];
+    [self reloadLayout];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated{
+    [self reloadLayout];
+}
+
+- (void)reloadLayout{
+    double webViewWidth = self.webView.frame.size.width;
+    double webViewHeight = webViewWidth * 0.5625;
+    [self.webView setFrame:(CGRectMake(0, 0, webViewWidth, webViewHeight))];
+    [self.titleLbl setFrame:(CGRectMake(self.titleLbl.frame.origin.x,
+                                        webViewHeight + 8,
+                                        self.titleLbl.frame.size.width,
+                                        self.titleLbl.frame.size.height))];
+    [self.descLbl setFrame:(CGRectMake(self.descLbl.frame.origin.x,
+                                       webViewHeight + 8 + self.titleLbl.frame.size.height + 8,
+                                       self.descLbl.frame.size.width,
+                                       self.descLbl.frame.size.height))];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+    NSString *css = @".container {position: relative; width: 100%; height: 0; padding-bottom: 56.25%;} .video{position:abosolute; top:0; left:0; width:100%; height:100%;}";
+    
+    NSString* js = [NSString stringWithFormat:
+                    @"var styleNode = document.createElement('style');\n"
+                    "styleNode.type = \"text/css\";\n"
+                    "var styleText = document.createTextNode('%@');\n"
+                    "styleNode.appendChild(styleText);\n"
+                    "document.getElementsByTagName('head')[0].appendChild(styleNode);\n",css];
+    
+    //NSLog(@"js:\n%@", js);
+    [self.webView stringByEvaluatingJavaScriptFromString:js];
 }
 
 /*
